78efe7424f8be39e8c9823d7660125676d16b112
Xu Chunyang <xuchunyang56@gmail.com>
2020-03-04 19:11:56 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# An Emacs Lisp Package Template
+
+Whether you write tests or not, you should at least setup a CI to ensure byte
+compilation does successes. Fortunately, it is very easy with GitHub Action,
+this repo demonstrates it.
---
+++ hello-world.el
@@ -0,0 +1,36 @@
+;;; hello-world.el --- Hello, World!                 -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Xu Chunyang
+
+;; Author: Xu Chunyang
+;; Homepage: https://github.com/xuchunyang/hello-world.el
+;; Keywords: tools
+;; Package-Requires: ((emacs "25.1"))
+;; Version: 0
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; The "Hello, World!" program.
+
+;;; Code:
+
+(defun hello-world ()
+  "Display \"Hello, World!\"."
+  (interactive)
+  (message "%s" "Hello, World!"))
+
+(provide 'hello-world)
+;;; hello-world.el ends here
